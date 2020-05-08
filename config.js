2e0a46279c581252c793d15374dd6f9d14e8fe52
latteyen <38789408+latteyen@users.noreply.github.com>
2020-05-08 13:14:49 +0700

    update bg

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/themomenthotel_chiangmai.jpg',
   pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
   // social
 
